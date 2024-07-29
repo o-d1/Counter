f5ee98ae9da7d9e296ceeeeca6d2517d737c8f25
o-d1 <oscar.davies109@gmail.com>
2024-07-29 18:17:22 +0100

    Almost finished, finished js and html, some css left to do.

--- counter.js
+++ counter.js
@@ -1,1 +1,32 @@
+const decrease = document.getElementById('decrease');
+decrease.addEventListener('click', decreaseFunction);
 
+const reset = document.getElementById('reset');
+reset.addEventListener('click', resetFunction);
+
+const increase = document.getElementById('increase');
+increase.addEventListener('click', increaseFunction);
+
+let userInput = document.getElementById('input');
+
+
+function decreaseFunction () {
+    userInput.value = parseFloat(userInput.value) - 1;
+}
+
+function increaseFunction () {
+    userInput.value = parseFloat(userInput.value) + 1;
+}
+
+function resetFunction () {
+    userInput.value = "";
+} 
+/*                                Was trying to make it into one function
+function buttonClciked () {
+    let userInput = document.getElementById('input');
+
+    if (increase.addEventListener('click')) {
+        userInput.value = parseFloat(userInput.value) + 1;
+    }
+}
+*/
